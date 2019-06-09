eff239bfecfbefc0f234c3d05f57f16041ef1d71
Oscar Sanchez <oscar.sanchez@orbis.com.pe>
2019-06-09 14:22:56 -0500

    feat(Development) if statement in entrypoint was added for development mode

--- docker/runtime/resources/runservices.sh
+++ docker/runtime/resources/runservices.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-source $VENV_DIR/bin/activate
+
+# for development mode
+if [[ ${DEVELOPMENT} ]]; then
+    source $VENV_DIR/bin/activate
+    echo "Development mode was enabled, this mode use virtualenv"
+fi
+
 gunicorn -c /resources/gunicorn.py wsgi:app
